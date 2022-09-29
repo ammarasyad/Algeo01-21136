0ed7921a9336dc5241a407c4eb9589a984485b58
Marthenn <bintang.dwimarthen@gmail.com>
2022-09-29 14:23:31 +0700

    refactor: merapikan file main

--- src/com/tubes/main/Main.java
+++ src/com/tubes/main/Main.java
@@ -1,8 +1,7 @@
+package com.tubes.main;
 import com.tubes.algeo.DoubleMatrix;
-import com.tubes.algeo.InterpolasiPolinomial;
 import com.tubes.algeo.Matrix;
 import com.tubes.algeo.MatrixFileOperator;
-import com.tubes.algeo.RegresiLinier;
 
 public class Main extends Menu{
     public static void main(String[] args){
@@ -33,9 +32,5 @@ public class Main extends Menu{
         // }
 
         //---------- BATAS SUCI PENGETESAN ----------
-        MatrixFileOperator mFile = MatrixFileOperator.getInstance();
-        DoubleMatrix m = mFile.createDMFromFile("D:\\Kuliah\\Semester 3\\AlGeo\\Algeo01-21136\\src\\a.txt");
-        double[] y = {0.9,0.91,0.96,0.89,1.00,1.10,1.15,1.03,0.77,1.07,1.07,0.94,1.10,1.10,1.10,0.91,0.87,0.78,0.82,0.95};
-        RegresiLinier.getSolution(m, y);
     }
 }
--- src/com/tubes/persoalan/RegresiLinier.java
+++ src/com/tubes/persoalan/RegresiLinier.java
@@ -61,4 +61,8 @@ public class RegresiLinier {
             }
         }
     }
+
+    public static void driver(){
+        System.out.println();
+    }
 }
