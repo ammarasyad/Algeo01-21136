350ecf5ef7748c340f1368ad65b2bf8a486a6c5c
ammarasyad <ammarasyad@gmail.com>
2022-09-29 13:06:37 +0700

    fix: Fix Gauss-Jordan Inverse's augmented matrix

--- src/com/tubes/algeo/MatrixOperators.java
+++ src/com/tubes/algeo/MatrixOperators.java
@@ -203,8 +203,12 @@ public final class MatrixOperators {
             }
             case GAUSS_JORDAN -> {
                 Matrix<Double> identity = Matrix.getIdentityMatrix(m.getRow());
-
-                return gaussJordan(m);
+                for (int i = 0; i < copy.getRow(); i++) {
+                    for (int j = 0; j < identity.getCol(); j++) {
+                        copy.getMatrix().get(i).add(identity.getElement(i, j));
+                    }
+                }
+                return gaussJordan(copy);
             }
         }
         return null;
