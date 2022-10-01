00bef90cc5619e45150b9a137059485bef5e34ee
Marthenn <bintang.dwimarthen@gmail.com>
2022-10-02 05:09:25 +0700

    feat: driverDeterminan, driverBalikan, driverRegresi, dan driverPolinomial selesai, kerangka driverSPL sudah ada

--- src/com/tubes/main/Driver.java
+++ src/com/tubes/main/Driver.java
@@ -9,48 +9,153 @@ public class Driver {
     static Scanner sc = new Scanner(System.in);
     static MatrixOperators mOps = MatrixOperators.getInstance();
 
+    protected static void printGauss(DoubleMatrix m){
+
+    }
+
     protected static void driverSPL(){
+        DoubleMatrix mat;
+        if(IOHandler.inputFile()){
+            mat = IOHandler.fileDoubleMatrix();
+        }
+        else{
+            System.out.print("Masukkan jumlah persamaan:\n> ");
+            int row = sc.nextInt();
+            System.out.print("Masukkan jumlah peubah:\n> ");
+            int col = sc.nextInt();
+            col++;
+            mat = new DoubleMatrix(row,col);
+            for(int i=0;i<row;i++){
+                for(int j=0;j<col-1;j++){
+                    System.out.printf("Masukkan nilai dari a%d,%d\n> ",i+1,j+1);
+                    double x = sc.nextDouble();
+                    mat.setElement(i, j, x);
+                }
+                System.out.printf("Masukkan nilai dari b%d\n> ",i+1);
+                double x = sc.nextDouble();
+                mat.setElement(i, col-1, x);
+            }
+        }
+        double[] res;DoubleMatrix mRes;
         Menu.menuSPL();
         int pilihan = IOHandler.opsi(1,4);
         switch(pilihan){
             case 1->{
                 //SPL Gauss
+                System.out.println("HASIL DARI SPL TERSEBUT DENGAN METODE GAUSS");
+                mRes = mOps.gauss(mat);
+                if(IOHandler.fileOutput()){
+                    //output file
+                }
             }
             case 2->{
                 //SPL Gauss-Jordan
+                System.out.println("HASIL DARI SPL TERSEBUT DENGAN METODE GAUSS-JORDAN");
+                mRes = mOps.gaussJordan(mat);
+                DoubleMatrix.printMatrix(mRes);
             }
             case 3->{
                 //SPL Inverse
             }
             case 4->{
                 //SPL Cramer
+                System.out.println("HASIL DARI SPL TERSEBUT DENGAN METODE CRAMER");
+                res = mOps.cramer(mat);
             }
         }
     }
 
     protected static void driverDeterminan(){
+        DoubleMatrix mat;
+        double res=0D;
+        if(IOHandler.inputFile()){
+            mat = IOHandler.fileDoubleMatrix();
+        }
+        else{
+            System.out.print("Masukkan dimensi matriks (square matriks)\n> ");
+            int n = sc.nextInt();
+            mat = new DoubleMatrix(n);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    System.out.printf("Masukkan nilai a%d,%d\n> ",i+1,j+1);
+                    double x = sc.nextDouble();
+                    mat.setElement(i, j, x);
+                }
+            }
+        }
+        System.out.println("Matriks yang diinput:");
+        DoubleMatrix.printMatrix(mat);
         Menu.menuDeterminan();
         int pilihan = IOHandler.opsi(1,2);
         switch(pilihan){
-            case 1->{
-                //Determinan Gauss
+            case 1->{ //reduksi baris
+                res = mOps.determinant(mat,MatrixOperators.ROW_REDUCTION);
             }
-            case 2->{
-                //Kofaktor expansion
+            case 2->{ //ekspansi kofaktor
+                res = mOps.determinant(mat,MatrixOperators.COFACTOR_EXPANSION);
+            }
+        }
+        System.out.println("Determinan dari matriks tersebut:");
+        System.out.println(res);
+        if(IOHandler.fileOutput()){
+            try{
+                String path = IOHandler.outputFile();
+                FileWriter fw = new FileWriter(path);
+                fw.write("Determinan dar matriks tersebut:\n");
+                fw.write(Double.toString(res));
+                fw.close();
+            }
+            catch(IOException error){
+                System.out.println("Error!");
             }
         }
     }
 
     protected static void driverBalikan(){
+        DoubleMatrix mat,res=new DoubleMatrix(0);
+        if(IOHandler.inputFile()){
+            mat = IOHandler.fileDoubleMatrix();
+        }
+        else{
+            System.out.print("Masukkan dimensi matriks (square matriks)\n> ");
+            int n = sc.nextInt();
+            mat = new DoubleMatrix(n);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    System.out.printf("Masukkan nilai a%d,%d\n> ",i+1,j+1);
+                    double x = sc.nextDouble();
+                    mat.setElement(i, j, x);
+                }
+            }
+        }
+        System.out.println("Matriks yang diinput:");
+        DoubleMatrix.printMatrix(mat);
         Menu.menuBalikan();
         int pilihan = IOHandler.opsi(1,2);
         switch(pilihan){
-            case 1->{
-                //Inverse Gauss-Jordan
+            case 1->{ //balikan metode matriks adjoin
+                res = mOps.inverse(mat, MatrixOperators.CLASSIC);
             }
-            case 2->{
-                //Inverse Kofaktor
+            case 2->{ //balikan metode gauss-jordan
+                DoubleMatrix temp = mOps.inverse(mat,MatrixOperators.GAUSS_JORDAN);
+                res = new DoubleMatrix(temp.getRow());
+                for(int i=0;i<temp.getRow();i++){
+                    for(int j=temp.getRow();j<temp.getCol();j++){
+                        res.setElement(i, j-temp.getRow(), temp.getElement(i, j));
+                    }
+                }
             }
+        }
+        if(res==null){
+            System.out.println("Matriks tidak memiliki balikan");
+        }
+        else{
+            System.out.println("Balikan dari matriks tersebut:");
+            DoubleMatrix.printMatrix(res);
+        }
+        if(IOHandler.fileOutput()){
+            String path = IOHandler.outputFile();
+            MatrixFileOperator.writeMatrixToFile(path, res);
         }
     }
 
@@ -71,10 +176,10 @@ public class Driver {
         System.out.println("Apakah ingin mengtaksir suatu nilai?\n1. Ya\n2. Tidak");
         int lanjut = IOHandler.opsi(1,2);
         if(lanjut==1){
-            System.out.print("Masukkan nilai dari x yang ingin ditaksir: ");
+            System.out.print("Masukkan nilai dari x yang ingin ditaksir\n> ");
             x = sc.nextDouble();
             res = InterpolasiPolinomial.getEstimate(coeff, x);
-            System.out.printf("Hasil estimasi dari p(%f): %f\n",x,res);
+            System.out.printf("Hasil estimasi dari f(%f): %f\n",x,res);
         }
         //Output file
         if(IOHandler.fileOutput()){
@@ -95,7 +200,7 @@ public class Driver {
                         }
                     }
                     if(lanjut==1){
-                        fw.write("\nHasil estimasi dari p("+Double.toString(x)+"): "+Double.toString(res));
+                        fw.write("\nHasil estimasi dari f("+Double.toString(x)+"): "+Double.toString(res));
                     }
                     fw.close();
                     System.out.println("Berhasil output pada "+path);
@@ -137,7 +242,7 @@ public class Driver {
         if(lanjut == 1){
             //Input Nilai Peubah ke List
             double[] peubah = new double[res.length-1];
-            System.out.printf("Masukkan nilai dari %d peubahnya\n",res.length-1);
+            System.out.printf("Masukkan nilai dari %d peubahnya\n> ",res.length-1);
             for(int i=0;i<res.length-1;i++){
                 peubah[i]=sc.nextDouble();
             }
